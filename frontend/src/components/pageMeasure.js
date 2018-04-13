b1f4cf6ab82c5f9ce16885742cbe333ebddf65de
Franz-Benjamin Mocnik <mail@franz-benjamin.net>
2018-04-13 19:28:22 +0200

    minor bug

--- frontend/src/components/pageMeasure.js
+++ frontend/src/components/pageMeasure.js
@@ -22,7 +22,7 @@ class PageMeasure extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      measures: [],
+      measures: {},
       search: '',
       errors: null,
     }
