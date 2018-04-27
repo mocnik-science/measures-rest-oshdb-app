f31bf3f2b33cd54af9c1707b667f6f820f99262e
Franz-Benjamin Mocnik <mail@franz-benjamin.net>
2018-04-27 19:01:47 +0200

    minor bug

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,7 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import {BrowserRouter} from 'react-router-dom'
 import App from './app'
-import registerServiceWorker from './service/registerServiceWorker'
 
 ReactDOM.render(
   <BrowserRouter>
