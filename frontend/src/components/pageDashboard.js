044bec90d346a9132a9807eda8865e0a1956b25f
Franz-Benjamin Mocnik <mail@franz-benjamin.net>
2018-05-01 12:49:27 +0200

    dashboard improved

--- frontend/src/components/pageDashboard.js
+++ frontend/src/components/pageDashboard.js
@@ -5,17 +5,23 @@ import Meter from 'grommet/components/Meter'
 import Value from 'grommet/components/Value'
 // import WorldMap from 'grommet/components/WorldMap'
 
-import {measures} from './../backend'
+import {items} from './../backend'
 
-const MAX_MEASURES = 100
+const MAX_CONTEXTS = 50
+const MAX_MEASURES = 50
+const MAX_PERSONS = 50
+const MAX_RESULTS = 50
 
 class PageDashboard extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      measures: {},
+      contexts: [],
+      measures: [],
+      persons: [],
+      results: [],
     }
-    measures(response => this.setState(response))
+    items(response => this.setState(response))
   }
   render() {
     return (
@@ -24,27 +30,92 @@ class PageDashboard extends React.Component {
         {/*
         <h2>Measures</h2>
         */}
-        <Box>
-          <Value
-            value={Object.keys(this.state.measures).length / MAX_MEASURES * 100}
-            units='measures'
-            size='xsmall'
-            align='start'
-          />
-          <Meter
-            value={Object.keys(this.state.measures).length / MAX_MEASURES * 100}
-            size='small'
-          />
-          <Box
-            direction='row'
-            justify='between'
-            pad={{between: 'small'}}
-            responsive={false}
-          >
-            <Label size='small'>0</Label>
-            <Label size='small'>{MAX_MEASURES}</Label>
+        <div style={{display: 'flex', flexFlow: 'row'}}>
+          <Box pad="medium">
+            <Value
+              value={Object.keys(this.state.measures).length}
+              units='measures'
+              size='xsmall'
+              align='start'
+            />
+            <Meter
+              value={Object.keys(this.state.measures).length / MAX_MEASURES * 100}
+              size='small'
+            />
+            <Box
+              direction='row'
+              justify='between'
+              pad={{between: 'small'}}
+              responsive={false}
+            >
+              <Label size='small'>0</Label>
+              <Label size='small'>{MAX_MEASURES}</Label>
+            </Box>
           </Box>
-        </Box>
+          <Box pad="medium">
+            <Value
+              value={Object.keys(this.state.results).length}
+              units='results'
+              size='xsmall'
+              align='start'
+            />
+            <Meter
+              value={Object.keys(this.state.results).length / MAX_RESULTS * 100}
+              size='small'
+            />
+            <Box
+              direction='row'
+              justify='between'
+              pad={{between: 'small'}}
+              responsive={false}
+            >
+              <Label size='small'>0</Label>
+              <Label size='small'>{MAX_RESULTS}</Label>
+            </Box>
+          </Box>
+          <Box pad="medium">
+            <Value
+              value={Object.keys(this.state.contexts).length}
+              units='contexts'
+              size='xsmall'
+              align='start'
+            />
+            <Meter
+              value={Object.keys(this.state.contexts).length / MAX_CONTEXTS * 100}
+              size='small'
+            />
+            <Box
+              direction='row'
+              justify='between'
+              pad={{between: 'small'}}
+              responsive={false}
+            >
+              <Label size='small'>0</Label>
+              <Label size='small'>{MAX_CONTEXTS}</Label>
+            </Box>
+          </Box>
+          <Box pad="medium">
+            <Value
+              value={Object.keys(this.state.persons).length}
+              units='people'
+              size='xsmall'
+              align='start'
+            />
+            <Meter
+              value={Object.keys(this.state.persons).length / MAX_PERSONS * 100}
+              size='small'
+            />
+            <Box
+              direction='row'
+              justify='between'
+              pad={{between: 'small'}}
+              responsive={false}
+            >
+              <Label size='small'>0</Label>
+              <Label size='small'>{MAX_PERSONS}</Label>
+            </Box>
+          </Box>
+        </div>
         {/*
         <h2 style={{marginTop: 80}}>Datasets</h2>
         <WorldMap
