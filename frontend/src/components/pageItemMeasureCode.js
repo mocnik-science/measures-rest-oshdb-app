500dd2e7099b9b5b5608f98e16a38ddb78d9caf9
Franz-Benjamin Mocnik <mail@franz-benjamin.net>
2018-05-02 22:10:06 +0200

    code cleanup

--- frontend/src/components/pageItemMeasureCode.js
+++ frontend/src/components/pageItemMeasureCode.js
@@ -1,5 +1,5 @@
-import React from 'react'
 import PropTypes from 'prop-types'
+import React from 'react'
 import Box from 'grommet/components/Box'
 import Button from 'grommet/components/Button'
 import Header from 'grommet/components/Header'
