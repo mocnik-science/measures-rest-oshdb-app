1542b886df9a45819e9e5281c7f4ffe622cf7989
Franz-Benjamin Mocnik <mail@franz-benjamin.net>
2018-05-07 02:50:40 +0200

    code autocompletion improved; warnings and errors shown

--- frontend/src/components/pageItemMeasureCode.js
+++ frontend/src/components/pageItemMeasureCode.js
@@ -2,26 +2,62 @@ import PropTypes from 'prop-types'
 import React from 'react'
 import Box from 'grommet/components/Box'
 import Button from 'grommet/components/Button'
+import Footer from 'grommet/components/Footer'
 import Header from 'grommet/components/Header'
 import Heading from 'grommet/components/Heading'
 import FontAwesomeIcon from '@fortawesome/react-fontawesome'
 import faCertificate from '@fortawesome/fontawesome-free-solid/faCertificate'
+import faExclamationTriangle from '@fortawesome/fontawesome-free-solid/faExclamationTriangle'
 import moment from 'moment'
 import MonacoEditor from 'react-monaco-editor'
+import soap from 'simplified-oshdb-api-programming/dist/soap-to-measure'
 
 import {item, itemSave} from './../backend'
 import {isLevelPublic} from './../tools'
 
+const AUTOCOMPLETE_DIRECTIVES = [
+  'snapshots //',
+  'contributions //',
+  'now //',
+  '####-##-## //',
+  'last day //',
+  'last month //',
+  'last year //',
+  'last # days //',
+  'last # months //',
+  'last # years //',
+  'daily //',
+  'monthly //',
+  'yearly //',
+  'every # days //',
+  'every # months //',
+  'every # years //',
+]
 const SOAP_METHODS = [
   'osmTypes(',
-  'where(',
+  'osmEntityFilter(',
+  'osmTag(',
   'map(',
+  'flatMap(',
   'filter(',
-  'average(',
-  'sum()',
-  'count()',
-  'uniq()',
+  'groupByEntity(',
+  'aggregateBy(',
+  'aggregateByGeometry(',
   'reduce(',
+  'sum(',
+  'count(',
+  'uniq(',
+  'countUniq(',
+  'average(',
+  'weightedAverage(',
+  'collect(',
+  'lineageBy(',
+]
+const SOAP_AGGREGATION_METHODS = [
+  'min',
+  'max',
+  'average',
+  'saturation',
 ]
 const AUTOCOMPLETE_OBJECT = [
   // types
@@ -49,12 +85,14 @@ class PageMeasureCode extends React.Component {
       lastSaved: null,
       lastSavedTry: null,
       buttonLabel: 'saved',
-      editorHeight: window.innerHeight - HEIGHT_HEADER,
+      windowHeight: window.innerHeight,
+      parsingErrors: [],
     }
     this.editorWillMount = this.editorWillMount.bind(this)
     this.editorDidMount = this.editorDidMount.bind(this)
     this.onResize = this.onResize.bind(this)
     this.onChange = this.onChange.bind(this)
+    this.onChangeCursorPosition = this.onChangeCursorPosition.bind(this)
     this.save = this.save.bind(this)
   }
   componentDidMount() {
@@ -66,18 +104,24 @@ class PageMeasureCode extends React.Component {
     clearInterval(this.saveService)
   }
   editorWillMount(monaco) {
+    this._monaco = monaco
     monaco.languages.registerCompletionItemProvider('java', {
-      triggerCharacters: ['.', ' ', '\n', '('],
+      triggerCharacters: ['.', ' ', '\n', '(', '// '],
       provideCompletionItems: (model, position, token, context) => {
         if (position.column < 3) return []
-        const c = model.getValueInRange({startLineNumber: position.lineNumber, startColumn: position.column - 1, endLineNumber: position.lineNumber, endColumn: position.column})
-        return ((c === '.') ? AUTOCOMPLETE_METHOD : AUTOCOMPLETE_OBJECT).map(l => ({label: l}))
+        const matches = t => (t === model.getValueInRange({startLineNumber: position.lineNumber, startColumn: position.column - t.length, endLineNumber: position.lineNumber, endColumn: position.column}))
+        if (matches('// ')) return AUTOCOMPLETE_DIRECTIVES.map(l => ({label: l}))
+        if (matches('lineageBy(')) return SOAP_AGGREGATION_METHODS.map(l => ({label: l}))
+        if (matches('.')) return AUTOCOMPLETE_METHOD.map(l => ({label: l}))
+        return AUTOCOMPLETE_OBJECT.map(l => ({label: l}))
       },
     })
   }
   editorDidMount(editor, monaco) {
     this._editor = editor
     editor.focus()
+    editor.onDidChangeCursorPosition(this.onChangeCursorPosition)
+    this.onChangeCursorPosition(null)
   }
   onResize() {
     this.setState({editorHeight: window.innerHeight - HEIGHT_HEADER})
@@ -85,6 +129,20 @@ class PageMeasureCode extends React.Component {
   }
   onChange(newValue, e) {
     this.setState({saved: false, code: newValue, lastSaved: null, lastSavedTry: null, buttonLabel: 'save'})
+  }
+  onChangeCursorPosition(e) {
+    if (this._editorDecorations === undefined || this._editorDecorations === null) this._editorDecorations = []
+    if (e === null || (this.state.parsingErrors && this.state.parsingErrors.length) || this._lastSelectedLine !== e.position.lineNumber) {
+      let parsedSoap = soap.soapToMeasureWithWarnings(this.state.code)
+      parsedSoap.errors = parsedSoap.errors.concat(parsedSoap.warnings)
+      this.setState({parsingErrors: parsedSoap.errors}, () => this._editor.layout())
+      const decorationList = parsedSoap.errors.filter(err => err[0] !== null).map(err => ({
+        range: new this._monaco.Range(Math.max(0, err[0] - 1), 1, err[0], 1),
+        options: {isWholeLine: true, linesDecorationsClassName: 'decorationError'},
+      }))
+      this._editorDecorations = this._editor.deltaDecorations(this._editorDecorations, decorationList)
+    }
+    this._lastSelectedLine = (e === null) ? null : e.position.lineNumber
   }
   save(force=false) {
     if (this.state.saved) return
@@ -129,7 +187,7 @@ class PageMeasureCode extends React.Component {
           }
         </Header>
         <MonacoEditor
-          height={this.state.editorHeight}
+          height={this.state.windowHeight - ((this.state.parsingErrors && this.state.parsingErrors.length) ? 2 : 1) * HEIGHT_HEADER}
           language='java'
           theme='vs-dark'
           value={this.state.code}
@@ -147,6 +205,13 @@ class PageMeasureCode extends React.Component {
             paths: {'vs': '/static/vs'},
           }}
         />
+        {
+          (this.state.parsingErrors && this.state.parsingErrors.length) ?
+          <Footer pad='medium'>
+            <FontAwesomeIcon icon={faExclamationTriangle} style={{fontSize: 24, marginRight: 14, marginBottom: 3, color: '#b81623'}}/>
+            {this.state.parsingErrors.map(e => e[1]).join('\n')}
+          </Footer> : []
+        }
       </Box>
     )
   }
