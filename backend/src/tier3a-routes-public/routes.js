db42be7100b7d29c382b8002f84a643982b83780
Franz-Benjamin Mocnik <mail@franz-benjamin.net>
2018-05-08 12:08:26 +0200

    bug

--- backend/src/tier3a-routes-public/routes.js
+++ backend/src/tier3a-routes-public/routes.js
@@ -4,12 +4,12 @@ const C = require('./../constants')
 
 module.exports.runRoutesPublic = (use, get, post) => {
   // static
-  use('/static/vs', express.static('./../../frontend/node_modules/monaco-editor/min/vs'))
-  use('/static/libs', express.static('./../../backend/libs'))
-  use('/static/help', express.static('./../../backend/help'))
+  use('/static/vs', express.static('./../frontend/node_modules/monaco-editor/min/vs'))
+  use('/static/libs', express.static('./../backend/libs'))
+  use('/static/help', express.static('./../backend/help'))
   
   if (!C.DEVELOPMENT) {
-    use('/', express.static('./../../frontend/build'))
-    use('*', express.static('./../../frontend/build/index.html'))
+    use('/', express.static('./../frontend/build'))
+    use('*', express.static('./../frontend/build/index.html'))
   }  
 }
