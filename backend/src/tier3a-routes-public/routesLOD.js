f620c1e0dbdc8a9de8ce59524b4e4a7a3d8b5724
Franz-Benjamin Mocnik <mail@franz-benjamin.net>
2018-05-11 01:40:47 +0200

    code cleanup

--- backend/src/tier3a-routes-public/routesLOD.js
+++ backend/src/tier3a-routes-public/routesLOD.js
@@ -6,72 +6,13 @@ const {itemForUser} = require('./../tier4-functionality/items')
 const {typesFromMeasure, isOSM, isQualityMeasure, nameForTypesOfMeasure, groundingsFromMeasure, tagsFromMeasure, listToPersonList, forenameToForenameShort, defaultData} = require('./../tier4-functionality/lod')
 
 module.exports.runRoutesPublicLOD = (use, get, post) => {
+  get('/repository/measure/:id', getMeasure(null))
+  get('/repository/result/:id', getResult(null))
+  get('/repository/context/:id', getContext(null))
+  get('/repository/person/:id', getPerson(null))
   use('/repository/static/', express.static(C.PATH_TEMPLATES_LOD_STATIC))
-  get('/repository/measure/:id', (req, res) => {
-    const item = itemForUser(C.MEASURE, null, req.params.id)
-    const types = typesFromMeasure(item)
-    res.render('measure', Object.assign(defaultData(req), {
-      title: item.name,
-      itemMeta: {
-        uri: `${settingsApp.repositoryUrl}/measure/${item.id}`,
-        type: types.join(' '),
-        itemTitle: item.name,
-        itemTypeTitle: nameForTypesOfMeasure(types),
-        isOSM: isOSM(item),
-        isQualityMeasure: isQualityMeasure(item),
-        implementedBy: listToPersonList(item.implementedBy),
-        documentedBy: listToPersonList(item.documentedBy),
-        api: {
-          prefix: 'http://',
-          main: 'purl.org/osm-data-quality/measure/measure-test/grid',
-          suffix: '?resolution={resolution}&bbox={bounding box}',
-        },
-        usesGrounding: groundingsFromMeasure(item),
-        assessesTag: tagsFromMeasure(item),
-      },
-      item: item,
-    }))
-  })
-  get('/repository/result/:id', (req, res) => {
-    const item = itemForUser(C.RESULT, null, req.params.id)
-    res.render('result', Object.assign(defaultData(req), {
-      title: item.name,
-      itemMeta: {
-        uri: `${settingsApp.repositoryUrl}/result/${item.id}`,
-        type: 'dq:result',
-        itemTitle: item.name,
-        itemTypeTitle: 'Result',
-        documentedBy: listToPersonList(item.documentedBy),
-      },
-      item: item,
-    }))
-  })
-  get('/repository/context/:id', (req, res) => {
-    const item = itemForUser(C.CONTEXT, null, req.params.id)
-    res.render('context', Object.assign(defaultData(req), {
-      title: item.name,
-      itemMeta: {
-        uri: `${settingsApp.repositoryUrl}/context/${item.id}`,
-        type: 'dq:context',
-        itemTitle: item.name,
-        itemTypeTitle: 'Context',
-        documentedBy: listToPersonList(item.documentedBy),
-      },
-      item: item,
-    }))
-  })
-  get('/repository/person/:id', (req, res) => {
-    const item = itemForUser(C.PERSON, null, req.params.id)
-    res.render('person', Object.assign(defaultData(req), {
-      title: item.name,
-      type: 'foaf:person',
-      itemMeta: {
-        uri: `${settingsApp.repositoryUrl}/person/${item.id}`,
-        itemTitle: item.name,
-        itemTypeTitle: 'Person',
-      },
-      item: item,
-      license: null,
+  get('/repository', (req, res) => {
+    res.render('home', Object.assign(defaultData(req, true), {
     }))
   })
 }
--- backend/src/tier4-functionality/lod.js
+++ backend/src/tier4-functionality/lod.js
@@ -2,18 +2,90 @@ const url = require('url')
 
 const C = require('./../constants')
 const settingsApp = require('./../settings')
-const {itemForUser, allItemsShort} = require('./../tier4-functionality/items')
+const {itemForUser, allItemsShort} = require('./items')
 
-module.exports.typesFromMeasure = measure => {
+// ROUTES //
+
+module.exports.getMeasure = user => (req, res) => {
+  const item = itemForUser(C.MEASURE, user, req.params.id)
+  const types = typesFromMeasure(item)
+  res.render('measure', Object.assign(defaultData(req), {
+    title: item.name,
+    itemMeta: {
+      uri: `${settingsApp.repositoryUrl}/measure/${item.id}`,
+      type: types.join(' '),
+      itemTitle: item.name,
+      itemTypeTitle: nameForTypesOfMeasure(types),
+      isOSM: isOSM(item),
+      isQualityMeasure: isQualityMeasure(item),
+      implementedBy: listToPersonList(item.implementedBy),
+      documentedBy: listToPersonList(item.documentedBy),
+      api: {
+        prefix: 'http://',
+        main: 'purl.org/osm-data-quality/measure/measure-test/grid',
+        suffix: '?resolution={resolution}&bbox={bounding box}',
+      },
+      usesGrounding: groundingsFromMeasure(item),
+      assessesTag: tagsFromMeasure(item),
+    },
+    item: item,
+  }))
+}
+module.exports.getResult = user => (req, res) => {
+  const item = itemForUser(C.RESULT, user, req.params.id)
+  res.render('result', Object.assign(defaultData(req), {
+    title: item.name,
+    itemMeta: {
+      uri: `${settingsApp.repositoryUrl}/result/${item.id}`,
+      type: 'dq:result',
+      itemTitle: item.name,
+      itemTypeTitle: 'Result',
+      documentedBy: listToPersonList(item.documentedBy),
+    },
+    item: item,
+  }))
+}
+module.exports.getContext = user => (req, res) => {
+  const item = itemForUser(C.CONTEXT, user, req.params.id)
+  res.render('context', Object.assign(defaultData(req), {
+    title: item.name,
+    itemMeta: {
+      uri: `${settingsApp.repositoryUrl}/context/${item.id}`,
+      type: 'dq:context',
+      itemTitle: item.name,
+      itemTypeTitle: 'Context',
+      documentedBy: listToPersonList(item.documentedBy),
+    },
+    item: item,
+  }))
+}
+module.exports.getPerson = user => (req, res) => {
+  const item = itemForUser(C.PERSON, user, req.params.id)
+  res.render('person', Object.assign(defaultData(req), {
+    title: item.name,
+    type: 'foaf:person',
+    itemMeta: {
+      uri: `${settingsApp.repositoryUrl}/person/${item.id}`,
+      itemTitle: item.name,
+      itemTypeTitle: 'Person',
+    },
+    item: item,
+    license: null,
+  }))
+}
+
+// HELPING FUNCTIONS //
+
+const typesFromMeasure = measure => {
   let types = (isOSM(measure)) ? ['dq:measure', 'dq:spatialMeasure', 'osmdq:spatialMeasure'] : ['dq:measure', 'dq:spatialMeasure']
   if (isQualityMeasure(measure)) return types = types.concat((isOSM(measure)) ? 'osmdq:spatialDataQualityMeasure' : 'dq:dataQualityMeasure')
   return types
 }
 
-module.exports.isOSM = isOSM = measure => measure.appliesToDataset && measure.appliesToDataset.id === 'osmdq:OpenStreetMap'
-module.exports.isQualityMeasure = isQualityMeasure = measure => (measure.usesGrounding.length > 0)
+const isOSM = measure => measure.appliesToDataset && measure.appliesToDataset.id === 'osmdq:OpenStreetMap'
+const isQualityMeasure = measure => (measure.usesGrounding.length > 0)
 
-module.exports.nameForTypesOfMeasure = types => {
+const nameForTypesOfMeasure = types => {
   if (types.includes('osmdq:spatialDataQualityMeasure')) return 'OpenStreetMap Data Quality Measure'
   if (types.includes('osmdq:spatialMeasure')) return 'OpenStreetMap Measure'
   if (types.includes('dq:dataQualityMeasure')) return 'Data Quality Measure'
@@ -21,27 +93,31 @@ module.exports.nameForTypesOfMeasure = types => {
   return ''
 }
 
-module.exports.groundingsFromMeasure = measure => measure.usesGrounding.map(g => ({
+const groundingsFromMeasure = measure => measure.usesGrounding.map(g => ({
   id: g.id,
   label: C.LOD_GROUNDING_DICTIONARY[g.id],
 }))
 
-module.exports.tagsFromMeasure = measure => measure.assessesTag.split(/[,;]/).map(t => t.match(/ *"?([^"=]*)"? *(= *"?([^"=]*)"? *)?/)).map(t => ({
+const tagsFromMeasure = measure => measure.assessesTag.split(/[,;]/).map(t => t.match(/ *"?([^"=]*)"? *(= *"?([^"=]*)"? *)?/)).map(t => ({
   key: t[1].trim(),
   value: (t[3]) ? t[3].trim() : undefined,
 }))
 
-module.exports.listToPersonList = list => {
+const listToPersonList = list => {
   const result = list.map(p => itemForUser(C.PERSON, null, p.id)).filter(p => p)
   result.map(p => p.forenameShort = forenameToForenameShort(p.forename))
   return result
 }
 
-module.exports.forenameToForenameShort = forenameToForenameShort = forename => forename.replace(/[a-zß-ÿ]/g, '')
+const forenameToForenameShort = forename => forename.replace(/[a-zß-ÿ]/g, '')
 
-module.exports.defaultData = req => {
+const defaultData = (req, withTypes=false) => {
   const items = []
-  C.ITEM_CLASSES.map(c => items.push(Object.assign({list: allItemsShort(c.path)}, c)))
+  C.ITEM_CLASSES.map(c => {
+    const items = allItemsShort(c.path)
+    if (withTypes) console.log(items)
+    items.push(Object.assign({list: items}, c))
+  })
   return {
     home: {
       titleLong: C.REPOSITORY_NAME_LONG,
