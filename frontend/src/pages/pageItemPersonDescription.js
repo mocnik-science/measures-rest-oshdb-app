32857a8eb7673f8be48bc71c0e2dfad6ddfc9094
Franz-Benjamin Mocnik <mail@franz-benjamin.net>
2018-05-10 13:25:22 +0200

    item person has now forename and surname

--- frontend/src/pages/pageItemPersonDescription.js
+++ frontend/src/pages/pageItemPersonDescription.js
@@ -4,12 +4,26 @@ import TextInput from 'grommet/components/TextInput'
 
 import PageItemDescription from './pageItemDescription'
 
+const styles = {
+  formFieldRow: {
+    display: 'flex',
+  },
+  formField: {
+    marginRight: -1,
+    marginBottom: -1,
+  },
+  formFieldLast: {
+    marginBottom: -1,
+  },
+}
+
 class PageItemPersonDescription extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
       id: null,
-      name: '',
+      forename: '',
+      surname: '',
       nameError: null,
       homepage: '',
       homepageError: null,
@@ -21,16 +35,23 @@ class PageItemPersonDescription extends React.Component {
       <PageItemDescription
         setState={response => this.setState(response)}
         itemName='person'
-        name={this.state.name}
+        name={`${this.state.forename} ${this.state.surname}`}
         data={{
-          name: this.state.name,
+          name: `${this.state.forename} ${this.state.surname}`,
+          forename: this.state.forename,
+          surname: this.state.surname,
           homepage: this.state.homepage,
           level: this.state.level,
         }}
         fields={[
-          <FormField key='name' label='forename and surname' error={this.state.nameError}>
-            <TextInput value={this.state.name} onDOMChange={e => this.setState({name: e.target.value})}/>
-          </FormField>,
+          <div style={styles.formFieldRow}>
+            <FormField key='name' label='forename' error={this.state.nameError} style={styles.formField}>
+              <TextInput value={this.state.forename} onDOMChange={e => this.setState({forename: e.target.value})}/>
+            </FormField>
+            <FormField key='name' label='surname' error={this.state.nameError} style={styles.formFieldLast}>
+              <TextInput value={this.state.surname} onDOMChange={e => this.setState({surname: e.target.value})}/>
+            </FormField>
+          </div>,
           <FormField key='homepage' label='homepage' error={this.state.homepageError}>
             <TextInput value={this.state.homepage} onDOMChange={e => this.setState({homepage: e.target.value})}/>
           </FormField>,
