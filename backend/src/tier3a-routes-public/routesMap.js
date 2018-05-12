dfe604e9265a472345e295847fa6c157e8669675
Franz-Benjamin Mocnik <mail@franz-benjamin.net>
2018-05-12 15:41:03 +0200

    bug

--- backend/src/tier3a-routes-public/routesMap.js
+++ backend/src/tier3a-routes-public/routesMap.js
@@ -2,5 +2,5 @@ const {getMap} = require('./../tier4-functionality/maps')
 
 module.exports.runRoutesPublicMap = (use, get, post) => {
   // html map page
-  get('/map/:id', (req, res) => res.status(200).send(getMap(req, res, null, C.LEVEL_PUBLIC)))
+  get('/repository/map/:id', (req, res) => getMap(req, res, null, C.LEVEL_PUBLIC))
 }
