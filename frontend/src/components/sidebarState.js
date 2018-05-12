acf81e43631001956f4b7f038c99eeb51ddb9ef7
Franz-Benjamin Mocnik <mail@franz-benjamin.net>
2018-05-12 18:46:27 +0200

    sidebarState improved

--- frontend/src/components/sidebarState.js
+++ frontend/src/components/sidebarState.js
@@ -1,3 +1,4 @@
+import PropTypes from 'prop-types'
 import React from 'react'
 import Button from 'grommet/components/Button'
 import Footer from 'grommet/components/Footer'
@@ -19,25 +20,53 @@ class SidebarService extends React.Component {
       serviceState: null,
       serviceLogs: null,
       showLogsLayer: false,
+      waitingForResponse: false,
     }
     this.checkService = this.checkService.bind(this)
     this.renderService = this.renderService.bind(this)
+    this.startService = this.startService.bind(this)
+    this.stopService = this.stopService.bind(this)
+    this.waitForResponse = this.waitForResponse.bind(this)
+    this.unwaitForResponse = this.unwaitForResponse.bind(this)
   }
   componentWillMount() {
-    this.checkService = setInterval(this.checkService, 1000)
+    this._checkService = setInterval(this.checkService, this.props.checkServiceInterval)
   }
   componentWillUnmount() {
-    clearInterval(this.checkService)
+    clearInterval(this._checkService)
+    clearInterval(this._waitForResponse)
   }
   checkService() {
     serviceState(response => this.setState(response))
   }
+  startService() {
+    this.waitForResponse()
+    serviceStart(response => {
+      this.unwaitForResponse()
+      this.setState(response)
+    })
+  }
+  stopService() {
+    this.waitForResponse()
+    serviceStop(response => {
+      this.unwaitForResponse()
+      this.setState(response)
+    })
+  }
+  waitForResponse() {
+    this.setState({waitingForResponse: true})
+    this._waitForResponse = setInterval(this.unwaitForResponse, this.props.waitForResponseTimeout)
+  }
+  unwaitForResponse() {
+    this.setState({waitingForResponse: false})
+    this._waitForResponse = setInterval(this.unwaitForResponse, this.props.waitForResponseTimeout)
+  }
   renderService() {
     switch (this.state.serviceRunning) {
       case true:
-        return <Button icon={<FontAwesomeIcon icon={faStop}/>} label={<Label>{this.state.serviceState}</Label>} plain={true} onClick={() => serviceStop(response => this.setState(response))}/>
+        return <Button icon={<FontAwesomeIcon icon={faStop}/>} label={<Label>{this.state.serviceState}</Label>} plain={true} onClick={(this.state.waitingForResponse) ? null : this.stopService}/>
       case false:
-        return <Button icon={<FontAwesomeIcon icon={faPlay}/>} label={<Label>{this.state.serviceState}</Label>} plain={true} onClick={() => serviceStart(response => this.setState(response))}/>
+        return <Button icon={<FontAwesomeIcon icon={faPlay}/>} label={<Label>{this.state.serviceState}</Label>} plain={true} onClick={(this.state.waitingForResponse) ? null : this.startService}/>
       default:
         return []
     }
@@ -51,11 +80,19 @@ class SidebarService extends React.Component {
             <code style={{fontSize: 12, fontFamily: 'monospace', whiteSpace: 'pre'}}>{this.state.serviceLogs}</code>
           </Layer> : []}
           {this.state.serviceLogs ? <Button icon={<FontAwesomeIcon icon={faExclamationTriangle}/>} onClick={() => this.setState({showLogsLayer: true})}/> : []}
-          {this.state.serviceRunning ? <Button icon={<FontAwesomeIcon icon={faRedo}/>} style={{marginLeft: -10}} onClick={() => serviceStart(response => this.setState(response))}/> : []}
+          {this.state.serviceRunning ? <Button icon={<FontAwesomeIcon icon={faRedo}/>} style={{marginLeft: -10}} onClick={(this.state.waitingForResponse) ? null : this.startService}/> : []}
         </div>
       </Footer>
     )
   }
+}
+SidebarService.propTypes = {
+  checkServiceInterval: PropTypes.number,
+  waitForResponseTimeout: PropTypes.number,
+}
+SidebarService.defaultProps = {
+  checkServiceInterval: 1000,
+  waitForResponseTimeout: 5000,
 }
 
 export default SidebarService
