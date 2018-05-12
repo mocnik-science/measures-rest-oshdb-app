526219b22268e24a2c8e460462243c63f936a685
Franz-Benjamin Mocnik <mail@franz-benjamin.net>
2018-05-12 15:58:16 +0200

    minor bug

--- backend/src/tier3a-routes-public/routesStatic.js
+++ backend/src/tier3a-routes-public/routesStatic.js
@@ -3,5 +3,6 @@ const express = require('express')
 module.exports.runRoutesPublicStatic = (use, get, post) => {
   use('/static/vs', express.static('./../frontend/node_modules/monaco-editor/min/vs'))
   use('/static/libs', express.static('./../backend/libs'))
+  use('/repository/static/libs', express.static('./../backend/libs'))
   use('/static/docs', express.static('./../backend/docs'))
 }
