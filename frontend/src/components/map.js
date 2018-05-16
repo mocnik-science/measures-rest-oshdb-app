7811417d893b8af89d783df3cc51b9dbabf4ded7
Franz-Benjamin Mocnik <mail@franz-benjamin.net>
2018-05-16 23:59:31 +0200

    show parameter in map view

--- frontend/src/components/map.js
+++ frontend/src/components/map.js
@@ -5,19 +5,28 @@ class Map extends React.Component {
   constructor(props) {
     super(props)
     this._L = window.L
+    this.addIsea3h = this.addIsea3h.bind(this)
   }
   componentDidMount() {
     this._map = this._L.map('map').setView([57.7, 12.0], 10)
-    this._L.stamenTileLayer('toner-lite').addTo(this._map);
-    this._isea3h = this._L.isea3hLayer({
-      url: `${this.props.url}?bbox={bbox}&resolution={resolution}`,
-      urlLibs: '/static/libs',
-      colorProgressBar: '#b81623',
-    }).addTo(this._map)
+    this._L.stamenTileLayer('toner-lite').addTo(this._map)
+    this.addIsea3h()
+  }
+  componentDidUpdate(prevProps, prevState) {
+    if (prevProps.parameters !== this.props.parameters) this.addIsea3h()
   }
   componentWillUnmount() {
     this._map.removeLayer(this._isea3h)
     this._map.remove()
+  }
+  addIsea3h() {
+    if (this._isea3h) this._map.removeLayer(this._isea3h)
+    this._isea3h = this._L.isea3hLayer({
+      url: `${this.props.url}?bbox={bbox}&resolution={resolution}${Object.keys(this.props.parameters).map(k => `&${k}={${k}}`).join('')}`,
+      urlLibs: '/static/libs',
+      colorProgressBar: '#b81623',
+      parameters: this.props.parameters,
+    }).addTo(this._map)
   }
   render() {
     return (<div id='map' style={{height: '100%'}}></div>)
@@ -25,6 +34,10 @@ class Map extends React.Component {
 }
 Map.propTypes = {
   url: PropTypes.string.isRequired,
+  parameters: PropTypes.object,
+}
+Map.defaultProps = {
+  parameters: {},
 }
 
 export default Map
--- frontend/src/pages/pageMap.js
+++ frontend/src/pages/pageMap.js
@@ -1,8 +1,11 @@
 import PropTypes from 'prop-types'
 import React from 'react'
 import Box from 'grommet/components/Box'
+import FormField from 'grommet/components/FormField'
 import Split from 'grommet/components/Split'
+import TextInput from 'grommet/components/TextInput'
 import Title from 'grommet/components/Title'
+import soap from 'simplified-oshdb-api-programming'
 
 import {mapInfo} from './../other/backend'
 import Map from './../components/map'
@@ -13,10 +16,24 @@ class PageMap extends React.Component {
     this.state = {
       measure: null,
       url: null,
+      parameters: {},
+      activeParameters: {},
     }
-    mapInfo(this.props.match.params.level, this.props.match.params.id, response => this.setState(response))
+    mapInfo(this.props.match.params.level, this.props.match.params.id, response => {
+      response.parameters = {}
+      const parsedSoap = soap.soapToMeasureWithWarnings(response.measure.code)
+      if (parsedSoap.parameters !== undefined) Object.values(parsedSoap.parameters).map(p => response.parameters[p.name] = p.defaultValue)
+      response.activeParameters = Object.assign({}, response.parameters)
+      this.setState(response)
+    })
+    this.onBlur = this.onBlur.bind(this)
+  }
+  onBlur() {
+    if (JSON.stringify(this.state.parameters) === JSON.stringify(this.state.activeParameters)) return
+    this.setState({activeParameters: Object.assign({}, this.state.parameters)})
   }
   render() {
+    const parsedSoap = (this.state.measure) ? soap.soapToMeasureWithWarnings(this.state.measure.code) : null
     return (
       <div className='map-split'>
         <Split flex='left' separator={true}>
@@ -26,11 +43,24 @@ class PageMap extends React.Component {
               <Map
                 key='map'
                 url={this.state.url}
+                parameters={this.state.activeParameters}
               />,
-              <Box key='sidebar' pad='medium' style={{width: 300}}>
-                <Title>{this.state.measure.name}</Title>
-                <p><b>You will soon find functionality here for analyzing the data.</b></p>
-                <p>{this.state.measure.description}</p>
+              <Box key='sidebar' pad='small' style={{width: 300}}>
+                <Title style={{marginBottom: 20}}>{this.state.measure.name}</Title>
+                {
+                  (!this.state.measure) ? [] :
+                  Object.values(parsedSoap.parameters).map(p => (
+                  <FormField
+                    key={p.name}
+                    label={p.name}
+                  >
+                    <TextInput
+                      defaultValue={this.state.parameters[p.name]}
+                      onDOMChange={e => this.setState({parameters: Object.assign(this.state.parameters, {[p.name]: e.target.value})})}
+                      onBlur={() => this.onBlur()}
+                    />
+                  </FormField>))
+                }
               </Box>
             ] : []
           }
